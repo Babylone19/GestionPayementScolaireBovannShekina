ee6ae9e31ff8019f85a90dd7d49c0b9105b34e96
Babylone19 <patrik.mayou@ifnti.com>
2025-09-30 15:04:12 +0200

    /..

--- bovann-frontend/src/api/auth.ts
+++ bovann-frontend/src/api/auth.ts
@@ -3,7 +3,7 @@ import { LoginCredentials } from '../types/user';
 
 // TOUJOURS utiliser localhost depuis le navigateur
 const API_BASE = 'https://gestionpayementscolairebovannshekina.onrender.com/api/auth';
-
+// const API_BASE = 'http://localhost:3000/api/auth';
 export const login = async (credentials: LoginCredentials): Promise<{ token: string; user: { id: string; email: string; role: string } }> => {
   const response = await axios.post(`${API_BASE}/login`, credentials);
   return response.data;
